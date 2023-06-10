25ae7ddb350cb647f58ea75773d89a42128ca90e
Darshna <darshnakhatri989@gmail.com>
2023-06-11 00:56:20 +0530

    edited

---
+++ src/components/Render.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Render =()=>{
+    return (
+        <div>
+            <p>I am learning React. My life is getting better.</p>
+        </div>
+    )
+}
+export default Render
